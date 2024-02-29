55a884cbb38e440f3d4e87ee0f400adcef6366ab
Elvis <egannem@gmail.com>
2024-02-29 17:55:56 -0300

    feat: Add product_sales table migration and ProductSale model
    Added migration file to create 'product_sales' table with fields product_id and sale_id. Also, created the ProductSale model to represent the product_sale entity.

---
+++ app/Models/ProductSale.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class ProductSale extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'product_id',
+        'sale_id'
+    ];
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function sale(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
