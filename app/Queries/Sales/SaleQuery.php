c51a8ea204b8d3b089324a1c9a88fc61ce802e00
Elvis <egannem@gmail.com>
2024-02-29 18:56:18 -0300

    feat: Add SaleQuery to retrieve all sales or a specific sale

---
+++ app/Queries/Sales/SaleQuery.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Queries\Sales;
+
+use App\Models\Sale;
+use Illuminate\Database\Eloquent\Collection;
+
+class SaleQuery
+{
+    public function getProductsFromSale(?int $saleId = null): Collection
+    {
+        $query = Sale::with(['products' => function ($query) {
+            $query->select('products.id', 'name', 'price', 'product_sales.product_amount as amount');
+        }]);
+
+        if ($saleId !== null) {
+            $query->where('id', $saleId);
+        }
+
+        $sales = $query->get();
+
+        $sales->transform(function ($sale) {
+            $sale->amount = $sale->calculateTotalAmount();
+            return $sale;
+        });
+
+        return $sales;
+    }
+}
