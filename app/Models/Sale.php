7603f59ffbf8fbe537591af3500c228f64a1fca7
Elvis <egannem@gmail.com>
2024-02-29 19:38:41 -0300

    feat: Implement logic for handling products in sales

--- app/Models/Sale.php
+++ app/Models/Sale.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -20,5 +21,21 @@ class Sale extends Model
         return round($this->products->reduce(function ($total, $product) {
             return $total + ($product->price * $product->amount);
         }, 0), 2);
+    }
+
+    public function handleProduct(array $product, int $saleId): void
+    {
+        $existingProductSale = $this->products()->where('product_id', $product['id'])->first();
+
+        if ($existingProductSale) {
+            $existingProductSale->pivot->product_amount += $product['amount'];
+            $existingProductSale->pivot->updated_at = Carbon::now();
+            $existingProductSale->pivot->save();
+        } else {
+            $this->products()->attach($product['id'], [
+                'product_amount' => $product['amount'],
+                'created_at' => Carbon::now()
+            ]);
+        }
     }
 }
